f9cc2d0b78c254429ca4be5f8969aad771faaeb0
Emerson-create <62583451+Emerson-create@users.noreply.github.com>
2020-04-20 15:45:25 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 # Ponha aqui os dados do seu robozinho
-bottoken=''
+bottoken='YWFhMmUwZDgtYWUzYi00YTM3LTkxYzgtN2U0MTY3ZWY2NzM0YThhYjJlZTEtNDM0_PF84_a892a6ed-b823-46b8-8336-847d8f4722ae'
 #exemplo='Yia0dsfdsfsdf...' - tirado do site developer.webex.com
-botmail=""
+botmail="iotboot@webex.bot"
 #exemplo='mybotmail@spark.io' - tirado do site developer.webex.com
 
 # Ponha aqui os dados do seu Webhook
 # webhook_url o endereco publico onde esta o app
-webhook_url=""
+webhook_url="https://iotboot.herokuapp.com"
 # exemplo webhook_url="https://mywebhook.com"
 # webhook_name e' o nome do gatilho que o Webex Teams gera para seu aplicativo entender
-webhook_name=""
+webhook_name="iotboot"
 # exemplo webhook_name="meuprimeirochatbot"
