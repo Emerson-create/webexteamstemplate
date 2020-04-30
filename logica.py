22515fedb2e12525c7ed3ae5ff8512d359f49986
Emerson Possamai <62583451+Emerson-create@users.noreply.github.com>
2020-04-30 10:34:40 -0300

    Add files via upload

--- logica.py
+++ logica.py
@@ -14,86 +14,11 @@ def logica(comando,usermail):
     #Separa o comando por espacos
     #Primeiro item e'o comando em si, os demais sao parametros deste comando
     #
-    
-    comando = comando.lower()
-    box=comando
-
-    while box == "oi" or box == "ola" or box == "hey" or box == "ei" or box == "alo" or box == "teste" or box =="Oi" or box == "Olá" or box == "help":
-        msg=""
-        arquivo=""
-        msg= "Olá Humano, antes de liberar o escoamento da água utilizada nos processos industriais, verifique comigo se o tanque especificado está pronto para voltar ao meio ambiente.\n" 
-        msg=msg+ "Qual das seguintes opções deseja ?\n"   
-        msg=msg+ "(1) - Temperatura atual da água\n"  
-        msg=msg+ "(2) - Buscar histórico de temperatura semanal\n"
-        msg=msg+ "(3) - Buscar histórico de temperatura mensal\n"
-        return msg,arquivo
-    else:
-        msg=""
-        arquivo=""
-        box2 = box
-        #condicional para temperatura da água
-        if box2 == "1":
-            msg="a temperatura atual da água é de xx graus."
-            return msg,arquivo
-        elif box2 == "agua":
-            msg="a temperatura atual da água é de xx graus."
-            return msg,arquivo
-        elif box2 == "temperatura":
-            msg="a temperatura atual da água é de xx graus"
-        elif box2 == "atual":
-            msg="a temperatura atual da água é de xx graus"
-            return msg,arquivo
-        elif box2 == "temperatura atual":
-            msg="a temperatura atual da água é de xx graus"
-            return msg,arquivo
-        #ajudinha antecedendo possíveis erros de digitação de temperatura atual 
-        elif box2 == "atula":
-            msg="As palavras que você digitou chegaram perto de 'atual' e 'temperatura atual', tente elas"
-            return msg,arquivo
-        elif box2 == "temperaturar":
-            msg="As palavras que você digitou chegaram perto de 'temperatura' e 'temperatura atual', tente elas"
-            return msg,arquivo
-        #histórico de temperatura semanal
-        elif box2 == "2":
-            msg="Segue o link para histórico de temperatura semanal"
-            return msg,arquivo
-        elif box2 == "histórico semanal":
-            msg="Segue o link para histórico de temperatura semanal"
-            return msg,arquivo
-        elif box2 == "semanal":
-            msg="Segue o link para histórico de temperatura semanal"
-            return msg,arquivo
-        elif box2 == "temperatura semanal":
-            msg="Segue o link para histórico de temperatura semanal"
-            return msg,arquivo
-        #possíveis erros de digitação em histórico de temperatura semanal
-        elif box2 == "historico":
-            msg="A palavra que você digitou chegou perto de 'histórico' , 'histórico semanal' , 'histórico mensal', tente elas"
-            return msg,arquivo        
-        elif box2 == "semana":
-            msg="A palavras que você digitou chegou perto de 'semanal' , 'histórico semana', tente elas"
-            return msg,arquivo
-        #Histórico de temperatura mensal
-        elif box2 == "3" or box2 == "mensal":
-            msg= "Segue o link para histórico de temperatura mensal"
-            return msg,arquivo
-        elif box2 == "histórico mensal":
-            msg="Segue o link para histórico de temperatura mensal"
-        #Possiveis erros em histórico de temperatura mensal
-        elif box2 == "mes" or box2 == "mensa":
-            msg: "A palavra que você digitou chegou perto de 'mensal', 'temperatura mensal', tente elas"
-            return msg,arquivo
-
-        
-            
-
-
-
-    # sp=comando.split(" ")
-
-    # print(sp)
+    comando=comando.lower()
+    sp=comando.split(" ")
     
     # comando na variavel box, lower deixa em minusculo para normalizar
+    box=sp[0]
     
     # Para o caso de nenhum pedido coberto aqui
     mais="\nEscreva 'mais' para saber suas opções"
@@ -101,9 +26,9 @@ def logica(comando,usermail):
     # 21.11.19
     # variavel arquivo para o caso do bot devolver arquivos anexados
     
-    # arquivo=""
+    arquivo=""
     
-    # msg=""
+    msg=""
 	
     # chamadas de acordo com os parametros
 
@@ -111,56 +36,30 @@ def logica(comando,usermail):
     
     # Uso da funcao "mais"
 
-    print(comando)
-    print(box)
+    if box == "mais" and len(sp)<2:
+        msg="Descubra sobre nossas principais ferramentas para ajudá-lo. Escreva:\n"
+        msg=msg+"mais sobre Cliente: conheça nosso programa semanal Quint@s Quinze\n"
+        msg=msg+"mais sobre Demos: nossas ferramentas de demonstração\n"
+        msg=msg+"mais sobre Projetos: nossa ferramenta para ajudar no desenvolvimento de projetos\n"
+        msg=msg+"mais sobre Treinamento: nossas ferramentas e programação de capacitação\n"
+        msg=msg+"mais sobre Suporte: Abertura de Chamados no Cisco TAC\n"
+        msg=msg+"mais sobre Alertas: assine nossas newsletter de Produtos\n"
 
-    if box == "oi":
+
+    if len(sp)>2:
+        tema=sp[2]
+        msg=maissobre(tema)
         
-        
-        if box == "1":
-            msg="dancing"
-
-    elif box == "ei":
-        msg= "Olá Humano, antes de liberar o escoamento da água utilizada nos processos industriais, verifique comigo se o tanque especificado está pronto para voltar ao meio ambiente.\n" 
-        msg=msg+ "Qual das seguintes opções deseja ?\n"   
-        msg=msg+ "(1) - Temperatura atual da água\n"  
-        msg=msg+ "(2) - Buscar histórico de temperatura semanal\n"
-        msg=msg+ "(3) - Buscar histórico de temperatura mensal\n"
-
-    elif box == "ola":
-        msg= "Olá Humano, antes de liberar o escoamento da água utilizada nos processos industriais, verifique comigo se o tanque especificado está pronto para voltar ao meio ambiente.\n" 
-        msg=msg+ "Qual das seguintes opções deseja ?\n"   
-        msg=msg+ "(1) - Temperatura atual da água\n"  
-        msg=msg+ "(2) - Buscar histórico de temperatura semanal\n"
-        msg=msg+ "(3) - Buscar histórico de temperatura mensal\n"
-
-    elif box == "alo":
-        msg= "Olá Humano, antes de liberar o escoamento da água utilizada nos processos industriais, verifique comigo se o tanque especificado está pronto para voltar ao meio ambiente.\n" 
-        msg=msg+ "Qual das seguintes opções deseja ?\n"   
-        msg=msg+ "(1) - Temperatura atual da água\n"  
-        msg=msg+ "(2) - Buscar histórico de temperatura semanal\n"
-        msg=msg+ "(3) - Buscar histórico de temperatura mensal\n"
-
-    else:
-        msg="Não entendi o que você quis dizer, por favor tente dizer 'oi' pra mim"
-
-
-    #  if len(sp)>2:
-    #             tema=sp[1]
-    #             msg=maissobre(tema)
-    #             print(sp)
-    
-        
-    # # Funcoes que usam outras APIs
-    # if len(sp)>1 and box=="api":
-    #     # URL
-    #     site="apitesteexample.com"
-    #     # Parametro de autorizacao
-    #     token="123456"
-    #     msg=APICall(site,token)
+    # Funcoes que usam outras APIs
+    if len(sp)>1 and box=="api":
+        # URL
+        site="apitesteexample.com"
+        # Parametro de autorizacao
+        token="123456"
+        msg=APICall(site,token)
         
 
-    # return msg,arquivo
+    return msg,arquivo
 
 
 def trataPOST(content):
@@ -168,20 +67,18 @@ def trataPOST(content):
     # resposta as perguntas via webexteams
     # trata mensagem quando nao e' gerada pelo bot. Se nao e' bot, entao usuario
     try:     
-        if content['name']==webhook_name and content['data']['personEmail']!=botmail:
+        if (content['dashboardId']):
             # identifica id da mensagem
-            msg_id=(content['data']['id'])
+            msg=(content['message'])
             # identifica dados da mensagem
-            webextalk=getwebexMsg(msg_id)
-            usermail=webextalk[2]
-            mensagem=webextalk[0]
-            sala=webextalk[1]
+            url=(content['ruleUrl'])
+            salaparamandarmsg=getwebexRoomID ("Y2lzY29zcGFyazovL3VzL1JPT00vZGQ1MmFjMDItYjU5YS0zYzczLTk2NzktODJlYTgxYmIzNDA5")
 
             # executa a logica
             msg,arquivo=logica(mensagem,usermail)
         
             # Envia resposta na sala apropriada
-            webexmsgRoomviaID(sala,msg,arquivo)
+            webexmsgRoomviaID(salaparamandarmsg,msg,url,arquivo)
 
     except:
             print("POST nao reconhecido")
