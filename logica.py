bc159543794cb1f1c9f2f182a41d481cffd98fe5
Emerson Possamai <62583451+Emerson-create@users.noreply.github.com>
2020-05-02 19:48:43 -0300

    Add files via upload

--- logica.py
+++ logica.py
@@ -183,7 +183,7 @@ def trataPOST(content):
             # Envia resposta na sala apropriada
             webexmsgRoomviaID(sala,msg,arquivo)
 
-        if content['dashboardId']==3 and content['ruleName']==Alarme de Temperatura da Agua:
+        if content['dashboardId']==3:
             msg=(content['message'])
             url=(content['ruleUrl'])
             webexmsgRoomviaId(Y2lzY29zcGFyazovL3VzL1JPT00vZGQ1MmFjMDItYjU5YS0zYzczLTk2NzktODJlYTgxYmIzNDA5,msg,url)
