7f413d8701d6a44d8122fb5d47d63ed0b811241c
Emerson Possamai <62583451+Emerson-create@users.noreply.github.com>
2020-05-01 23:31:33 -0300

    Update logica.py

--- logica.py
+++ logica.py
@@ -183,17 +183,17 @@ def trataPOST(content):
             usermail=webextalk[2]
             mensagem=webextalk[0]
             sala=webextalk[1]
+	
+	 	if (content['dashboardId']):
+	        msg=(content['message'])
+	        url=(content['ruleUrl'])
+	        webexmsgRoomviaId(Y2lzY29zcGFyazovL3VzL1JPT00vZGQ1MmFjMDItYjU5YS0zYzczLTk2NzktODJlYTgxYmIzNDA5,msg,url)
 
             # executa a logica
             msg,arquivo=logica(mensagem,usermail)
         
             # Envia resposta na sala apropriada
             webexmsgRoomviaID(sala,msg,arquivo)
-
-            if (content['dashboardId']):
-	            msg=(content['message'])
-	            url=(content['ruleUrl'])
-	            webexmsgRoomviaId(Y2lzY29zcGFyazovL3VzL1JPT00vZGQ1MmFjMDItYjU5YS0zYzczLTk2NzktODJlYTgxYmIzNDA5,msg,url)
 
     except:
             print("POST nao reconhecido")
